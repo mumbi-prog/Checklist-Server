4c02f106e416cf3dc913bf8ca10440166ed0a343
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-17 04:48:57 +0300

    ChecklistItem associations and validations

--- app/models/checklist_item.rb
+++ app/models/checklist_item.rb
@@ -1,4 +1,7 @@
 class ChecklistItem < ApplicationRecord
-  belongs_to :item
+  belongs_to :Item
   belongs_to :checklist
+
+  validates :status, presence: true, inclusion: { in: %w[OK NOT_OK] }
+  validates :item_id, uniqueness: { scope: :checklist_id }
 end
