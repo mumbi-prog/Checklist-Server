2b97b672bd0e08d939ba225d1fc9cac3aa1808e0
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-17 05:09:48 +0300

    Added index operation in checklist

--- app/controllers/checklist_items_controller.rb
+++ app/controllers/checklist_items_controller.rb
@@ -1,2 +1,8 @@
 class ChecklistItemsController < ApplicationController
+
+    def index
+        checklists = Checklist.all
+        render json: checklists, include: :checklist_items
+    end
+    
 end
