009ff7c6fd2b98f4b20abd35f349c3c581fbfa59
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-17 05:01:38 +0300

    Created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+begin 
+
+ChecklistItem.delete_all
+Checklist.delete_all
+Item.delete_all
+Category.delete_all
+
+categories = {
+  'Server Backups' => ['DTL Master Backup', 'DTL Host Backup', 'Mohammed Folder Backup', 'Accounts Folder'],
+  'Printers' => ['Kyocera TaskAlfa 358ci', 'Kyocera TaskAlfa 4012i'],
+  'CCTV' => ['Entry Gate Projection', 'MD Screen Projection', 'Maintenance Projection', 'CCTV Backup'],
+  'Applications over the network' => ['SAP Business One All Users', 'Microsoft Office All Users', 'Email on All Users', 'Biometric Device Cock-ins Recorded'],
+  'Managing Directors Office' =>  ['Computer', 'Printer'],
+  'Server Room' => ['AC working okay', 'Firewall Monitor Logs', 'Kaspersky Security Center Logs'],
+  'Others' => ['Boardroom AC Working Okay', 'Mohammed AC Working Okay']
+}
+
+categories.each do |category_name, items|
+  category = Category.create!(name: category_name)
+  items.each do |item_name|
+    Item.create!(name: item_name, category: category)
+  end
+end
+
+Rails.logger.info("Data seeding was successful.")
+
+puts "Data seeding was successful."
+
+rescue => e
+Rails.logger.error("Data seeding failed: #{e.message}")
+
+puts "Data seeding failed: #{e.message}"
+end
