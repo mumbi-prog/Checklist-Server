2d862119cd534a71021062b84b2aae3d1508d852
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-17 04:47:31 +0300

    Checklist associations and validations

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,2 +1,6 @@
 class Checklist < ApplicationRecord
+    has_many :checklist_items, dependent: :destroy
+    has_many :items, through: :checklist_items
+
+    validates :date, presence: true, uniqueness: true
 end
