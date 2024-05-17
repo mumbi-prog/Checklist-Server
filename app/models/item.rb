21c7bd85c25151e959e85bc7a85896e0fc656715
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-17 04:46:42 +0300

    Item associations and validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
+  has_many :checklist_items
+  has_many :checklists, through: :checklist_items
+
+  validates :name, presence: true, uniqueness: { scope: :category_id }
 end
