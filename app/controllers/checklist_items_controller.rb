8076c5fc15cfef15890669a2a7bbef5a853348b1
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-17 05:10:59 +0300

    Added permitted params in checklist

--- app/controllers/checklist_items_controller.rb
+++ app/controllers/checklist_items_controller.rb
@@ -4,5 +4,16 @@ class ChecklistItemsController < ApplicationController
         checklists = Checklist.all
         render json: checklists, include: :checklist_items
     end
-    
+
+    def show
+        checklist = Checklist.find(params[:id])
+        render json: checklist, include: :checklist_items
+    end
+
+    private
+
+    def checklist_params
+      params.require(:checklist).permit(:date)
+    end
+
 end
