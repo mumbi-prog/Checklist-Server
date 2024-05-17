ef22549ab0a0eb4a59ca211e38ace49f233068a5
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-17 05:13:58 +0300

    Added create operation in checklist

--- app/controllers/checklist_items_controller.rb
+++ app/controllers/checklist_items_controller.rb
@@ -10,6 +10,19 @@ class ChecklistItemsController < ApplicationController
         render json: checklist, include: :checklist_items
     end
 
+    def create
+        checklist = Checklist.new(checklist_params)
+        if checklist.save
+          items = Item.all
+          items.each do |item|
+            checklist.checklist_items.create(item: item, status: params[:items][item.id.to_s][:status], remark: params[:items][item.id.to_s][:remark])
+          end
+          render json: checklist, include: :checklist_items, status: :created
+        else
+          render json: checklist.errors, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def checklist_params
