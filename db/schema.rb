588464a9732d2a9785cccd04aa315613be72f582
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-17 04:54:43 +0300

    Created database and ran migrations

---
+++ db/schema.rb
@@ -0,0 +1,57 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_05_17_014408) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
+  create_table "checklist_items", force: :cascade do |t|
+    t.string "status", null: false
+    t.text "remark"
+    t.bigint "item_id", null: false
+    t.bigint "checklist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["checklist_id"], name: "index_checklist_items_on_checklist_id"
+    t.index ["item_id", "checklist_id"], name: "index_checklist_items_on_item_id_and_checklist_id", unique: true
+    t.index ["item_id"], name: "index_checklist_items_on_item_id"
+  end
+
+  create_table "checklists", force: :cascade do |t|
+    t.date "date", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["date"], name: "index_checklists_on_date", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "status"
+    t.text "remark"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["name", "category_id"], name: "index_items_on_name_and_category_id", unique: true
+  end
+
+  add_foreign_key "checklist_items", "checklists"
+  add_foreign_key "checklist_items", "items"
+  add_foreign_key "items", "categories"
+end
