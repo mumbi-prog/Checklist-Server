07a2fb1490805593991d76b13af14d931f68ff53
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-18 07:02:18 +0300

    Corrected User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@dakawou\.com\z/i, message: "Use @dakawou.com email address" } 
-end
     validates :password, presence: true, length: { minimum: 6 }
 end
