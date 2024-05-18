1b0779fb28d20898d264ae0bf5c4fbd0f1111d78
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-18 06:58:55 +0300

    Defined create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,18 @@
 class SessionsController < ApplicationController
-   
+    def create
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: { status: 'logged_in', user: user }, status: :ok
+      else
+        render json: { status: 'error', message: 'Invalid email or password' }, status: :unauthorized
+      end
+    end
   
-  
+    def destroy
+      session[:user_id] = nil
+      render json: { status: 'logged_out' }, status: :ok
+    end
   
     def logged_in
       if logged_in?
