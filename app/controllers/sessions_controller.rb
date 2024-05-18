85c6cdf418b54eea65aa08a06f80103679e7d61f
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-18 06:49:29 +0300

    Created private method for current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,9 @@
 class SessionsController < ApplicationController
-end
+     
+    private
+  
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  end
+  
