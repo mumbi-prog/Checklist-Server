c2ef9d54231e118859f3b6616abe30a02919acb9
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-18 06:52:07 +0300

    Created methos to determine is user is logged in based on session id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,21 @@
 class SessionsController < ApplicationController
-     
+   
+  
+  
+  
+    def logged_in
+      if logged_in?
+        render json: { logged_in: true, user: current_user }
+      else
+        render json: { logged_in: false }
+      end
+    end
+  
     private
+  
+    def logged_in?
+      !!session[:user_id]
+    end
   
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
