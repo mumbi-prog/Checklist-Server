02d0d7757e1f69b4c5f3e083b352515727f7f834
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-18 14:40:02 +0300

    EnsureD that only authenticated users can perform crud actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception, prepend: true
-    
-end
+    protect_from_forgery with: :exception
+    before_action :require_login
+  
+    helper_method :current_user, :logged_in?
+  
+    private
+  
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  
+    def logged_in?
+      !!current_user
+    end
+  
+    def require_login
+      unless logged_in?
+        render json: { status: 'error', message: 'Unauthorized' }, status: :unauthorized
+      end
+    end
+  end
+  
