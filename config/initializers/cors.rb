a323c920e2ceeb87df15058ddd92fce18b33a5c4
mumbi <mumbi099@gmail.com>
2024-05-20 09:56:09 +0300

    Added deployed frontend link to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001','http://localhost:3002'
+      origins 'http://localhost:3001','http://localhost:3002','https://dtl-ict-checklist.vercel.app/'
   
       resource '*', 
         headers: :any,
