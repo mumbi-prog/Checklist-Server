d95e1a0c914a3bb96d85a975d25023d17fb287ae
mumbi <mumbi099@gmail.com>
2024-07-01 11:05:54 +0300

    Updates

--- db/migrate/20240518032554_create_users.rb
+++ db/migrate/20240518032554_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :email, null: false, unique: true
+      t.string :email, null: false
       t.string :password_digest, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -52,10 +52,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_18_032554) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "checklist_items", "checklists"
